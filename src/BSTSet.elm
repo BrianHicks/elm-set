99a508482570daf5db2969824a8d483085274252
Brian Hicks <brian@brianthicks.com>
2016-11-25 23:24:58 -0600

    BSTSet: add comments in balance

--- src/BSTSet.elm
+++ src/BSTSet.elm
@@ -189,14 +189,19 @@ balance set =
 
         Tree _ head left right ->
             if diff set == -2 && diff left == 1 then
+                -- left leaning tree with right-leaning left subtree. Rotate left, then right.
                 tree head (rotl left) right |> rotr
             else if diff set < -1 then
+                -- left leaning tree, generally. Rotate right.
                 rotr set
             else if diff set == 2 && diff right == -1 then
+                -- right leaning tree with left-leaning right subtree. Rotate right, then left.
                 tree head left (rotr right) |> rotl
             else if diff set > 1 then
+                -- right leaning tree, generally. Rotate left.
                 rotl set
             else
+                -- diff is -1, 0, or 1. Already balanced, no operation required.
                 set
 
 
